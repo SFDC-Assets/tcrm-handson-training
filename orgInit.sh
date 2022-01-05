3336b9a8d49399fe2ffb616c2e52c436935e979b
Jonathan Zhou <jonathan.zhou@jzhou-ltm1.internal.salesforce.com>
2022-01-05 14:45:35 -0500

    init

--- orgInit.sh
+++ orgInit.sh
@@ -1,8 +1,10 @@
-sfdx shane:org:create -f config/project-scratch-def.json -d 1 -s --userprefix event-monitoring -o work.shop
-sfdx shane:tsp:username:update
-sfdx force:package:install -p 04t1t000003DLALAA4 -w 10
+sfdx shane:org:create -f config/project-scratch-def.json -d 1 -s --userprefix einstein -o discovery.demo
+
+sfdx shane:analytics:dataset:upload -f data/AcquiredAccount.csv -m data/AcquiredAccount.json -n AcquiredAccount -a SharedApp --async
+sfdx shane:analytics:dataset:upload -f data/deliveries.csv -m data/deliveries.json -n deliveries -a SharedApp --async
+sfdx shane:analytics:dataset:upload -f data/testdrives.csv -m data/testdrives.json -n testdrives -a SharedApp --async
+sfdx shane:analytics:dataset:upload -f data/csat.csv -m data/csat.json -n csat -a SharedApp --async
+
 sfdx force:source:push
-sfdx force:user:permset:assign -n Event_Monitoring_Access
 sfdx shane:user:password:set -p salesforce1 -g User -l User
-sfdx force:user:create -a other-user
-sfdx force:org:open
+sfdx force:org:open -p /analytics/home
