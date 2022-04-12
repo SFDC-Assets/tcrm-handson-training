37f93e5b49da0ddfa588675825dcf6dc6cd22986
bgalla <bgalla@users.noreply.github.com>
2022-04-12 11:41:59 -0400

    Update orgInit.sh
    changed TelcoChurn folder to Share_Predictions_with_EDiT

--- orgInit.sh
+++ orgInit.sh
@@ -8,7 +8,7 @@ sfdx force:apex:execute -f scripts/apex/changeOwnerOpportunityRandom.apex
 sfdx force:apex:execute -f scripts/apex/changeOwnerEvent_Random.apex
 sfdx force:apex:execute -f scripts/apex/changeOwnerCase_Random.apex
 
-sfdx shane:analytics:dataset:upload -f data/churnmodern2020.csv -m data/churnmodern2020.json -n TelcoChurn -a SharedApp --async
+sfdx shane:analytics:dataset:upload -f data/churnmodern2020.csv -m data/churnmodern2020.json -n TelcoChurn -a Share_Predictions_with_EDiT --async
 sfdx shane:analytics:dataset:upload -f data/Account_Churn.csv -m data/Account_Churn.json -n 'Account_Churn' -a Build_a_Model_with_ED --async
 
 sfdx analytics:app:create -f analytics/CustomerInsights_template_Values.json --wait 60
