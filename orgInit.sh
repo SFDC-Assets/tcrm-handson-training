5827e905ed4149ded078f2e9967d877de4e4f21a
bgalla <bgalla@users.noreply.github.com>
2022-04-14 21:05:41 -0400

    Update orgInit.sh
    added in core data

--- orgInit.sh
+++ orgInit.sh
@@ -2,7 +2,7 @@ sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --userprefix e
 sfdx force:source:push
 
 sfdx shane:user:password:set -p salesforce1 -g User -l User
-
+sfdx automig:load  --inputdir ./data/core
 sfdx shane:analytics:dataset:upload -f data/churnmodern2020.csv -m data/churnmodern2020.json -n TelcoChurn -a Share_Predictions_with_EDiT --async
 sfdx shane:analytics:dataset:upload -f data/Account_Churn.csv -m data/Account_Churn.json -n 'Account_Churn' -a Build_a_Model_with_ED --async
 
