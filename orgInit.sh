cfef8f0341bc48b048110c0e9f051bd1dda84cea
bgalla <bgalla@users.noreply.github.com>
2023-08-17 17:03:27 -0400

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -5,6 +5,6 @@ sfdx shane:user:password:set -p salesforce1 -g User -l User
 sfdx automig:load  --inputdir ./data/core
 sfdx shane:analytics:dataset:upload -f data/churnmodern2020.csv -m data/churnmodern2020.json -n TelcoChurn -a Share_Predictions_with_EDiT --async
 sfdx shane:analytics:dataset:upload -f data/Account_Churn.csv -m data/Account_Churn.json -n 'Account_Churn' -a Build_a_Model_with_ED --async
-sfdx force:package:install --package="Tableau Viz Lightning Web Component"
+
 
 sfdx force:org:open --path lightning
